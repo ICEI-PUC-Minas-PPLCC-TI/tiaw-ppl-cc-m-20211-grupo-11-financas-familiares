7e7b1f1b50b0159605acb1c30e5036d94b73d1fb
Valadaress <matheus.valadaresm@gmail.com>
2021-05-19 23:23:24 -0300

    new page
    call a html page

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("HELLO TEST")
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    fs.readFile('home.html', function(err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
