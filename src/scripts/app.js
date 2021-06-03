a624aee3ef3cd8bc874b9ca7f9c568b14b332b0d
Valadaress <matheus.valadaresm@gmail.com>
2021-06-03 20:19:06 -0300

    s

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -16,6 +16,8 @@ server.listen(porta, '127.0.0.1')
 
 console.log('yo dawgs, now listening to port 127.0.0.1:3000')
 
+window.location.replace("../index.html")
+
 // var http = require('http');
 // var fs = require('fs');
 // var path = require('path');
