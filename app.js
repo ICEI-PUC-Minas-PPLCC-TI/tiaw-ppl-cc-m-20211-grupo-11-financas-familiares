c02c01d8ec89ab971b617ddf7d5f2bfd6234cbfa
Valadaress <matheus.valadaresm@gmail.com>
2021-06-02 21:53:42 -0300

    error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url)
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+    var myReadStream = fs.createReadStream(__dirname + 'index.html', 'utf8')
     myReadStream.pipe(res)
 })
 
@@ -13,3 +13,50 @@ server.listen(porta, '127.0.0.1')
 
 
 console.log('yo dawgs, now listening to port 3000')
+
+// var http = require('http');
+// var fs = require('fs');
+// var path = require('path');
+
+// http.createServer(function(request, response) {
+
+//     console.log('request starting for ');
+//     console.log(request);
+
+//     var filePath = '.' + request.url;
+//     if (filePath == './')
+//         filePath = 'index.html';
+
+//     console.log(filePath);
+//     var extname = path.extname(filePath);
+//     var contentType = 'text/html';
+//     switch (extname) {
+//         case '.js':
+//             contentType = 'text/javascript';
+//             break;
+//         case '.css':
+//             contentType = 'text/css';
+//             break;
+//     }
+
+//     path.exists(filePath, function(exists) {
+
+//         if (exists) {
+//             fs.readFile(filePath, function(error, content) {
+//                 if (error) {
+//                     response.writeHead(500);
+//                     response.end();
+//                 } else {
+//                     response.writeHead(200, { 'Content-Type': contentType });
+//                     response.end(content, 'utf-8');
+//                 }
+//             });
+//         } else {
+//             response.writeHead(404);
+//             response.end();
+//         }
+//     });
+
+// }).listen(5000);
+
+// console.log('Server running at http://127.0.0.1:5000/');
