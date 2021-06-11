16502eafb0583905a02491fc0c69573ed599428f
Valadaress <matheus.valadaresm@gmail.com>
2021-06-10 23:30:29 -0300

    s

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const projName = 'TI - AW';
 
-var http = require('http')
-var fs = require('fs')
+const express = require('express')
 
-var server = http.createServer(function(req, res) {
-    console.log('request was made: ' + req.url)
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    var myReadStream = fs.createReadStream('src/_index.html', 'utf8') //__dirname + 
-    myReadStream.pipe(res)
-})
+const app = express()
 
-var port = process.env.PORT || 8080
-    // server.listen(port, '0.0.0.0')
-server.listen(port, '127.0.0.1')
+const port = process.env.PORT || 8080
 
-console.log('yo dawgs, now listening to port 127.0.0.1:8080')
+app.use(express.static("public"))
+
+try {
+
+    app.listen(port, '127.0.0.1')
+    console.log('yo dawgs, now listening to port 127.0.0.1:8080')
+} catch (error) {
+
+    console.log(error)
+}
+
+
+
+
+// const http = require('http')
+// const fs = require('fs')
+
+// var server = http.createServer(function(req, res) {
+//     console.log('request was made: ' + req.url)
+//     res.writeHead(200, { 'Content-Type': 'text/html' })
+//     var myReadStream = fs.createReadStream('src/_index.html', 'utf8') //__dirname + 
+//     myReadStream.pipe(res)
+// })
+
+
+// server.listen(port, '0.0.0.0')
