88264413dcb2f7a426b63aa1713beb19e72bba0b
Valadaress <matheus.valadaresm@gmail.com>
2021-07-07 04:08:48 -0300

    login update

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,40 @@
-
+const objLogin = [
+    {
+        username: "test",
+        password: "123"
+    },
+    {
+        username: "test2",
+        password: "1234"
+    },
+    {
+        username: "Test3",
+        password: "321"
+    },
+]
 
 var modal = document.getElementById('id01');
 
-window.onload = () =>{
-    login.onsubmit = (evento) => {
-        console.log({ nome: usuario.value, senha: senha.value});
-        window.localStorage.setItem('username', usuario.value);
-        window.localStorage.setItem('password', senha.value);
+function login() {
+    var loc = window.location.pathname;
+    var loc2 = loc.substring(1, loc.lastIndexOf('/'));
+    const dir = loc2 + "/index.html"
+    const username = document.getElementById("usuario").value
+    const password = document.getElementById("senha").value
+    const o = 0
+    
+    if (username != null || password != null) {       
+    
+    for (i = 0; i < objLogin.length; i++) {
+        if (username == objLogin[i].username && password == objLogin[i].password) {
+            document.getElementById('id01').style.display='none'
+            document.getElementById('h_id_user').innerHTML = 'Welcome'
+            return
+        }        
     }
+    alert("Incorrect name or password")
+}else {
+    alert("Please Fill the blank fields or input a valid value")
 }
-
-
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
 
